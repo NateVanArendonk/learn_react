ab5566129825a9ec8be517834d825572f0a4ee9e
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-05-18 15:11:44 -0700

    Add navbar styling

--- project-1-static-page/src/components/Main.js
+++ project-1-static-page/src/components/Main.js
@@ -2,6 +2,17 @@ import React from "react";
 
 export default function Main() {
     return (
-        <h1>Main Page</h1>
+        <main className="main-body">
+            <h1 className="main-header">
+                Fun facts about React
+            </h1>
+            <ul className="main-list">
+                <li className="list-item">Was first released in 2013</li>
+                <li className="list-item">Was originally created by Jordan Walke</li>
+                <li className="list-item">Has well over 100K stars on GitHub</li>
+                <li className="list-item">Is maintained by Facebook</li>
+                <li className="list-item">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
--- project-1-static-page/src/components/Navbar.js
+++ project-1-static-page/src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import logo from "../images/react-icon-small.png"
 
 export default function Navbar() {
     return (
-        <h1>Navbar</h1>
+        <nav className="navbar"> 
+            <img src={logo} className="nav-image"></img>
+            <h3 className="navbar-main-text">ReactFacts</h3>
+            <h4 className="navbar-sub-text">React Course - Project 1</h4>
+        </nav>
     )
 }
