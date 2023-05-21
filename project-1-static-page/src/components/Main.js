d086374f85cd345c9d9b851eee644ec95f732976
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-05-20 17:38:18 -0700

    Add styling to main page

--- project-1-static-page/src/components/Main.js
+++ project-1-static-page/src/components/Main.js
@@ -1,17 +1,15 @@
-import React from "react";
+import React from "react"
 
 export default function Main() {
     return (
-        <main className="main-body">
-            <h1 className="main-header">
-                Fun facts about React
-            </h1>
-            <ul className="main-list">
-                <li className="list-item">Was first released in 2013</li>
-                <li className="list-item">Was originally created by Jordan Walke</li>
-                <li className="list-item">Has well over 100K stars on GitHub</li>
-                <li className="list-item">Is maintained by Facebook</li>
-                <li className="list-item">Powers thousands of enterprise apps, including mobile apps</li>
+        <main>
+            <h1 className="main-head">Fun facts about React</h1>
+            <ul className="fact-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </main>
     )
